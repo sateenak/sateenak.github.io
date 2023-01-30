9aba0d24e36c1d5d762d56c50b8a35bf4bfc0160
mdwiastika <marceldwias@gmail.com>
2023-01-30 09:38:10 +0700

    first commit

---
+++ public/assets/js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function () {
+  // nav 3 lines animation
+  const nav_button = document.querySelector('.nav-button');
+  const nav = document.querySelector('nav');
+  nav_button.addEventListener('click', function () {
+    nav.classList.toggle('hidden');
+    nav_button.classList.toggle('nav-active');
+  });
+
+  //Blur navbar scroll
+  const header = document.querySelector('header');
+  window.addEventListener('scroll', function () {
+    if (this.scrollY > 1) {
+      header.classList.add('navbar-scroll');
+    } else {
+      header.classList.remove('navbar-scroll');
+    }
+  });
+  // search button
+  const search_box = document.querySelector('.search-box');
+  const search_button = document.querySelector('.search-button');
+  const cancel_button = document.querySelector('.cancel-button');
+  search_button.addEventListener('click', function () {
+    search_box.classList.add('search-active');
+  });
+
+  cancel_button.addEventListener('click', function () {
+    search_box.classList.remove('search-active');
+  });
+  // announcement
+  const announce = document.getElementById('announce');
+  const announce_button = document.getElementById('announce-button');
+  announce_button.addEventListener('click', function () {
+    announce.classList.remove('lg:block');
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      poppins: ['Poppins'],
+    },
+    extend: {
+      animation: {
+        wush: 'wush 0.3s linear forwards',
+        navItem: 'navItem 0.3s linear forwards',
+        listItemShow: 'listItemShow 0.4s linear forwards',
+        // typingIndicator: 'typingIndicator 0.8s steps(3) infinite',
+        // typing: 'typing 14s steps(14) infinite',
+      },
+    },
+  },
+  plugins: [],
+};
